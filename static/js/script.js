e2823ae270546090550d1a45cc80e1814aeedfea
Abubakir <ikrima84@gmail.com>
2024-08-19 17:45:55 +0600

    Remove School directory

--- static/js/script.js
+++ static/js/script.js
@@ -4,25 +4,25 @@ let b = document.querySelector('.container-section')
 let c = document.querySelector('.header')
 let d = document.querySelector('.nav')
 let g = document.querySelector('.register button')
+let v = document.querySelector('.container4')
 if (fullUrl == 'http://127.0.0.1:8000/news/index_new/' || fullUrl != 'http://127.0.0.1:8000/news/'){
     a.style.display = 'none'
     b.style.display = 'none'
     c.style.padding = '10px 20px 10px 0px'
     d.style.width = "699px"
     d.style.height = "16px"
-    g.style.padding = "3px 10px"
-    g.style.border = "3px solid"
-    g.style.color = "#FF7B01"
-    g.style.backgroundColor = 'white'
 }else {
     a.style.display = 'flex'
-    g.style.color = "white"
     d.style.width = "395px"
     b.style.display = 'flex'
-    g.style.backgroundColor = '#FF8C00'
     d.style.heigth = "34px"
-    g.style.border = "none"
-    g.style.padding = "10px 20px"
     c.style.padding = '10px 20px 10px 0px'
 }
 
+if (fullUrl === 'http://127.0.0.1:8000/workspace/register/') {
+    v.style.marginTop = '147px'
+    v.style.marginBottom = '150px'
+} else {
+    v.style.marginTop = '0px';
+    v.style.marginBottom = '0px'
+}
